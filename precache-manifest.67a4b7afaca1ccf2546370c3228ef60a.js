7c977a2fb1381d6ea3b3f00f9fd56821871a00c4
Abhishek Thakur <abhishek.thakur@crewkarma.com>
2020-04-13 21:24:36 +0530

    Updates

---
+++ precache-manifest.67a4b7afaca1ccf2546370c3228ef60a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c48d7db473ddd346d11430fd31e7da",
+    "url": "/CoronaBiharTracker/index.html"
+  },
+  {
+    "revision": "071d8b9ef55c4071b5b1",
+    "url": "/CoronaBiharTracker/static/js/2.a01f7364.chunk.js"
+  },
+  {
+    "revision": "2a6f7b0ac760b5d15fb8b482e9b9b618",
+    "url": "/CoronaBiharTracker/static/js/2.a01f7364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f018aee724dd3a32c9",
+    "url": "/CoronaBiharTracker/static/js/main.8ab3902b.chunk.js"
+  },
+  {
+    "revision": "7a21ab062abe2ffef1cd",
+    "url": "/CoronaBiharTracker/static/js/runtime-main.65d0bf60.js"
+  }
+]);
